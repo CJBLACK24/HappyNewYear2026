06f3a691b94b2b5cd4dcd016c8b0316461deb6c8
CJBLACK24 <cjblack24@users.noreply.github.com>
2025-12-31 21:04:54 +0800

    Fix Velocity Text direction: Opposite flows for rows

--- components/VelocityText.tsx
+++ components/VelocityText.tsx
@@ -19,11 +19,11 @@ export default function VelocityText() {
                     Advance Happy New Year 2026&nbsp;•&nbsp;
                 </ScrollVelocityRow>
 
-                {/* Row 2: Scrolling Left */}
+                {/* Row 2: Scrolling Left (Now Right per request) */}
                 <ScrollVelocityRow
                     baseVelocity={3}
                     className="text-5xl md:text-8xl font-black text-transparent py-4"
-                    direction={-1}
+                    direction={1}
                 >
                     <span className="stroke-text">Belated Merry Christmas&nbsp;•&nbsp;</span>
                 </ScrollVelocityRow>
